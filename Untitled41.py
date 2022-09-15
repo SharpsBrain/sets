b2c5cb0de7c5388f2d550a66287579b5d09eb0d3
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-09-15 11:04:00 +0500

    Add files via upload

---
+++ Untitled41.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+theset={"sharps", "brain", "channel"}
+print(len(theset))
+
+
+# In[2]:
+
+
+thisset={"sharps", "brain", "channel", "sharps", "brain", "channel"}
+
+
+# In[8]:
+
+
+set1={"sharps", "brain"}
+set2={1,5,6,7,8}
+set3={True, False, True}
+print(type(set1))
+      #set2,set3)
+
+
+# In[7]:
+
+
+set2={"sharps",558,True,40,"brain"}
+print(type(set2))
+
+
+# In[9]:
+
+
+thsset=set(("sharps","brain"))
+print(thsset)
+
+
+# In[ ]:
+
+
+
+
